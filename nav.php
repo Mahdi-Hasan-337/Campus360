c645622f038ca67307dae72a5b51651d042ba08e
MahdiHasanSadi <mahdi.lu.cse.53.h@gmail.com>
2023-07-22 13:09:30 +0600

    33

--- nav.php
+++ nav.php
@@ -151,9 +151,8 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-body">
-        <form action="helper/registerAction.php" method="post">
+        <form action="registerAction.php" method="post">
         <button type="button" class="modal-close-btn" style="" data-bs-dismiss="modal" aria-label="Close">&times</button>
-          <!-- <button type="button" class="modal-close-btn btn-close" style="margin-right:2rem; margin-top:2rem;" data-bs-dismiss="modal" aria-label="Close"></button> -->
           <h2>Register</h2>
           <div class="input-box">
             <input type="text" id="username" placeholder="Enter username" class="form-control" name="r_username" required>
@@ -164,13 +163,13 @@
               <i class="fa-solid fa-envelope email"></i>
           </div>
           <div class="input-box">
-              <input type="email" id="email" placeholder="Enter phone number" class="form-control" name="r_phone" required>
+              <input type="text" id="phone" placeholder="Enter phone number" class="form-control" name="r_phone" required>
               <i class="fa-solid fa-phone email"></i>
           </div>
           <div class="input-box">
-              <input type="password" placeholder="Enter Password" id="password" class="form-control" name="r_pass" required>
-              <i class="fa-solid fa-lock password"></i>
-              <i class="toggle-password fa-solid fa-eye-slash pwd_hide" onclick="togglePasswordVisibility('password')" style="color:#707070; font-size: 0.9rem;"></i>
+            <input type="password" placeholder="Enter Password" id="r_password" class="form-control" name="r_pass" required>
+            <i class="fa-solid fa-lock password"></i>
+            <i class="toggle-password fa-solid fa-eye-slash pwd_hide" onclick="togglePasswordVisibility('r_password')" style="color:#707070; font-size: 0.9rem;"></i>
           </div>
           <div class="input-box">
               <input type="password" placeholder="Re-type Password" id="con_password" class="form-control" name="r_con_pass" required>
--- registerAction.php
+++ registerAction.php
@@ -10,52 +10,52 @@
 
     // regex of all fields
     $username_pattern = "/[A-Za-z .]{3,20}/";
-    $email_pattern = "/(cse|eee|law)_\d{10}@lus\.ac\.bd/";
+    $email_pattern = "/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/";
     $phone_pattern = "/(\+88)?-?01[3-9]\d{8}/";
     $pass_pattern = "/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&*_+><])).{6,20}/";
 
     // To check username not used twice
-    $duplicate_username = mysqli_query($conn, "SELECT * FROM `register` WHERE db_username='$r_username'");
+    $duplicate_email = mysqli_query($conn, "SELECT * FROM `register` WHERE db_email='$r_email'");
 
-    if(mysqli_num_rows($duplicate_username) > 0)
+    if(mysqli_num_rows($duplicate_email) > 0)
     {
         echo "<script>alert('Username Already Taken')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     // to validate user name field
     elseif(!preg_match($username_pattern, $r_username))
     {
         echo "<script>alert('Invalid Username')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     // to validate email field
     elseif(!preg_match($email_pattern, $r_email))
     {
         echo "<script>alert('Invalid Email')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     // To validate phone field
     elseif(!preg_match($phone_pattern, $r_phone))
     {
         echo "<script>alert('Invalid Phone Number')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     // To check password field
     elseif(!preg_match($pass_pattern, $r_pass))
     {
         echo "<script>alert('Invalid Password')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     // to check confirm password field and password field are same or not
     elseif($r_pass!=$r_con_pass)
     {
         echo "<script>alert('Password not matched')</script>";
-        echo "<script>location.href='register.php'</script>";
+        echo "<script>location.href='index.php'</script>";
     }
 
     else
@@ -68,8 +68,7 @@
         else
         {
             echo "<script>alert('Inserted')</script>";
-            echo "<script>location.href='register.php'</script>";
+            echo "<script>location.href='index.php'</script>";
         }
     }
-
 ?>
